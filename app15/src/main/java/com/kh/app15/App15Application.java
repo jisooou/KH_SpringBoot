b5d8ff0859eb4319eb37d3478e9dca9041bfe5ae
DESKTOP-38924LQ\user1 <jisoou4@gmail.com>
2024-07-17 15:51:10 +0900

    EC2 : app15 커밋

---
+++ app15/src/main/java/com/kh/app15/App15Application.java
@@ -0,0 +1,13 @@
+package com.kh.app15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App15Application.class, args);
+	}
+
+}
---
+++ app15/src/test/java/com/kh/app15/App15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.app15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
